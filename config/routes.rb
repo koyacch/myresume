d300a4a6949082811ea0a279c35535a6198b427a
noriyoshi koyama <koyacch@gmail.com>
2018-01-10 08:00:21 +0000

    静的ファイルで仮ルーティング。テストの自動化。テストの作成と確認。

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static_pages#home'
+  get  '/myfeed',    to: 'static_pages#myfeed'
+  get  '/carrier',   to: 'static_pages#carrier'
+  get  '/contact', to: 'static_pages#contact'
+
 end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  test "should get home" do
+    get root_url
+    assert_response :success
+  end
+
+  test "should get myfeed" do
+    get myfeed_url
+    assert_response :success
+  end
+
+  test "should get carrier" do
+    get carrier_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get contact_url
+    assert_response :success
+  end
+
+end
