a0b2a117fe7519291466d6fe9ee15488abf8db7c
noriyoshi koyama <koyacch@gmail.com>
2018-01-25 07:54:41 +0000

    トップページのhtmlを作成。パーシャルで分割。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  # title tagを返すヘルパー
+  def tag_title(title="")
+    base_title = "MyResume"
+    if title.empty?
+      base_title
+    else
+      title + " | " + base_title
+    end
+  end
+      
 end
